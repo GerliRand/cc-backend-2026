4f21c93c6da8589680b94c1e2fc1826ab6f2c07b
Gerli Rand <gerli@hotmail.fi>
2026-03-10 15:13:52 +0200

    Add sentiment analysis model and scikit-learn dependency, and test API endpoint

--- api.py
+++ api.py
@@ -1,16 +1,50 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import pickle
 
 app = Flask(__name__)
 
-# read pickle file hier (add before the file)
+# Read pickle file (using Module 2 model)
+with open("sentiment_analysis_model.pkl", "rb") as file:
+    data = pickle.load(file)
 
+# Checking that the backend server is running
 @app.route("/")
-def hello_world():
-    return "<h2>Hello, World! My name is Gerli, whats your name?</h2>"
-
+def home():
+    return jsonify({
+        "message": "Backend is running..."
+    })
+   
+# api endpoint
 @app.route("/api", methods=["POST"])
 def sentiment():
-    body_data = request.get_json()
-    print("Body data:")
-    print(body_data["data"])
-    return {"message": "Hello from localproject - api!!!"}
+    body = request.get_json()
+
+    # Checking that it is the correct format
+    if not body:
+        return jsonify({
+            "error": "Request body must be JSON format "
+        }), 400
+
+    # Checking that the text field exists
+    if "text" not in body:
+        return jsonify({
+            "error": "Missing text field"
+        }), 400
+    
+    # Extract the text sent from the frontend
+    text = body["text"]
+
+    # Checking that the text is not empty
+    if text == "":
+        return jsonify({
+            "error": "Text field is empty"
+        })
+    
+    # Using the trained model to predict the sentiment of the text
+    prediction = data.predict([text])[0]
+    
+    # Return the original text and the predicted sentiment as JSON
+    return jsonify({
+        "text": text,
+        "prediction": prediction
+    })
